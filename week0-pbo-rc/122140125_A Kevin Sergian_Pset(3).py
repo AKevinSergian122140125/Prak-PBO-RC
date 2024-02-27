77c71b5383e368b9b4f34f3e7c24b21c46f2bdbe
AKevinSergian122140125 <161465624+AKevinSergian122140125@users.noreply.github.com>
2024-02-27 20:34:25 +0700

    Create 122140125_A Kevin Sergian_Pset(3).py

---
+++ week0-pbo-rc/122140125_A Kevin Sergian_Pset(3).py
@@ -0,0 +1,6 @@
+file = open("Me.txt","w")
+file.write("Nama   : A Kevin Sergian\n")
+file.write("NIM    : 122140125 \n")
+file.write("Resolusi Saya Tahun : Menjadi kaya dan mampu menghasilkan uang 'aamiin' ")
+
+print("Data disimpan kedalam Me.txt") 
